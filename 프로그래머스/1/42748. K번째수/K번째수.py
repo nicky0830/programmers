2076604f0b24ac484c1e304b8de21163614781fc
nicky <kwaknicky@naver.com>
2024-05-01 09:53:46 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for c in commands:
+        [i,j,k] = c
+        arr = array[i-1:j]
+        arr.sort()
+        answer.append(arr[k-1])
+    return answer
