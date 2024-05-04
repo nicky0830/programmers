594d2861f65941f3d40567df74475212393fdc6d
nicky <kwaknicky@naver.com>
2024-05-04 16:18:42 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.03 ms
+메모리: 10.1 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 30일 18:17:32
+2024년 05월 04일 16:18:39
 
 ### 문제 설명
 
--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,13 +1,13 @@
 def solution(num):
-    answer = 0
-    if num == 1:
-        return 0
-    while num > 1:
-        if num % 2 == 0:
-            num = num // 2
-        else:
-            num = num * 3 + 1
-        answer +=1
-        if answer == 500:
-            return -1
-    return answer
+    return dfs(num, 0)
+
+def dfs(n, answer):
+    if answer == 500:
+        return -1
+    if n == 1:
+        return answer
+    
+    if n % 2 == 0:
+        return dfs(int(n/2), answer+1)
+    else:
+        return dfs(int(n*3+1), answer+1)
