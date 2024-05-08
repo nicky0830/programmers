8d2e7ca9701476e45de875e1b1acd4d16133bad8
nicky <kwaknicky@naver.com>
2024-05-08 15:33:24 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,8 @@
+def solution(date1, date2):
+    for i in range(len(date1)):
+        if date1[i] < date2[i]:
+            return 1
+        if date1[i] > date2[i]:
+            return 0
+    return 0
+    
