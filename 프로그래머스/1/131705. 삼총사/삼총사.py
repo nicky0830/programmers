db3a5ad18f7e3952314cabe986ca105a682fdf97
nicky <kwaknicky@naver.com>
2024-05-29 15:10:07 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,9 +1,7 @@
+from itertools import combinations
 def solution(number):
-    answer = 0 
-    l = len(number)
-    for i in range(l):
-        for j in range(i+1, l):
-            for k in range(j+1, l):
-                if number[i] + number[j] + number[k] == 0:
-                    answer += 1
+    answer = 0
+    for i in combinations(number,3):
+        if sum(i) == 0:
+            answer += 1
     return answer
