43834d3a7c21ff0d6b0fff535ba56a51999a6ab4
nicky <kwaknicky@naver.com>
2024-06-03 15:27:02 +0900

    [level 2] Title: 숫자의 표현, Time: 3.71 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        summ = 0
+        while summ < n:
+            summ += i
+            i += 1
+        if summ == n:
+            answer += 1
+    return answer
