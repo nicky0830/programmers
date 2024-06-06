3c632b16671f935757f5e7a2ba05151a8ca4818a
nicky <kwaknicky@naver.com>
2024-06-06 16:04:00 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 02일 10:36:23
+2024년 06월 06일 16:03:57
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,9 +1,9 @@
 def solution(n, lost, reserve):
-    lost.sort()
-    reserve.sort()
-    common = set(lost) & set(reserve)
-    lost = list(set(lost) - common)
-    for r in list(set(reserve) - common):
+    answer = 0
+    common = set(reserve) & set(lost)
+    reserve = sorted(list(set(reserve) - set(common)))
+    lost = sorted(list(set(lost) - set(common)))
+    for r in reserve:
         if r-1 in lost:
             lost.remove(r-1)
         elif r+1 in lost:
