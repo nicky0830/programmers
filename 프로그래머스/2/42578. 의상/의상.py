ffd7be5b7560f746bed753a6efad723f4ecc3f38
nicky <kwaknicky@naver.com>
2024-08-10 22:57:22 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,8 @@
 def solution(clothes):
     answer = 1
-    dic = {}
-    for [cloth,kind] in clothes:
-        if kind in dic:
-            dic[kind] += 1
-        else:
-            dic[kind] = 1
-    for value in dic.values():
-        answer *= (value + 1)
-    return answer -1 
+    dic = {c[1]: [] for c in clothes}
+    for c in clothes:
+        dic[c[1]].append(c[0])
+    for k,v in dic.items():
+        answer *= (len(v) + 1)
+    return answer - 1
