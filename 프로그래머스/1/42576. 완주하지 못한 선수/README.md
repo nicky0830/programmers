4445881be33ba5a64d0a30b0eab0c72f0c95447e
nicky <kwaknicky@naver.com>
2024-08-10 16:36:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 60.14 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 38.6 MB, 시간: 56.02 ms
+메모리: 33.9 MB, 시간: 60.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 22일 19:20:36
+2024년 08월 10일 16:36:06
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,12 @@
+from collections import Counter
 def solution(participant, completion):
-    answer = ''
-    p_dic = {i:0 for i in participant}
-    c_dic = {i:0 for i in participant}
-    for p in participant:
-        p_dic[p] += 1
+    count = Counter(participant)
     for c in completion:
-        c_dic[c] += 1
+        if count[c] >.0:
+            count[c] -= 1
+    for k,v in count.items():
+        if v>0:
+            return k
+
         
-    for p in participant:
-        if p_dic[p] - c_dic[p] == 1:
-            return p
+
