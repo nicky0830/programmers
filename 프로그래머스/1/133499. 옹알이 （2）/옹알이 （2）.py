28dfee24553cadb3ba99234c215c0d3f6caf4d27
nicky <kwaknicky@naver.com>
2024-08-19 13:07:26 +0900

    [level 1] Title: 옹알이 (2), Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,15 +1,17 @@
 def solution(babbling):
     answer = 0
-    sound = ['aya', 'ye', 'woo', 'ma']
-    wrong_bable = ['ayaaya', 'yeye', 'woowoo', 'mama']
-    
+    not_ = ["ayaaya", "yeye", "woowoo", "mama"]
+    yes = ["aya", "ye", "woo", "ma"]
     for b in babbling:
-        for w in wrong_bable:
-            if w in b:
-                b = 'X'
-        for s in sound:
-            b = b.replace(s, ' ')
+        for n in not_:
+            if n in b:
+                b = b.replace(n, 'x')
+# 연속으로 나오는 거 먼저 모두 제거 (이 단계에서 하나씩 나오는 것도 제거하면 안됨)
+        for y in yes:
+            b = b.replace(y, ' ')
+# 연속이 아니라 띄엄띄엄 나오는 단어들 모두 삭제 위해 그대로 사용
         b = b.replace(' ', '')
         if len(b) == 0:
-            answer+=1
+            answer +=1
+    
     return answer
