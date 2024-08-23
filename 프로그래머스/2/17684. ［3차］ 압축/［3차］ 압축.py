88de95ff2e60160d7c1a19a3813e73b1b41d766c
nicky <kwaknicky@naver.com>
2024-08-23 15:23:31 +0900

    [level 2] Title: [3차] 압축, Time: 0.80 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,19 +1,33 @@
 from string import ascii_uppercase
 def solution(msg):
-    dic = {a: i+1 for i,a in enumerate(ascii_uppercase)}
-    last = len(ascii_uppercase)+1
     answer = []
-    pass_i = []
-    for i in range(len(msg)):
-        if i in pass_i:
-            continue
-        for j in range(len(msg), i, -1):
-            letter = msg[i:j]
-            if letter in dic:
-                for k in range(i+1, j):
-                    pass_i.append(k)
-                answer.append(dic[letter])
-                dic[msg[i:j+1]] = last
-                last += 1
+    d = {a:i+1 for i, a in enumerate(ascii_uppercase)}
+    while True:
+        if msg in d:
+            answer.append(d[msg])
+            break
+        for i in range(1,len(msg)+1):
+            if msg[0:i] not in d:
+                answer.append(d[msg[0:i-1]])
+                d[msg[0:i]] = len(d) + 1
+                msg = msg[i-1:]
                 break
     return answer
+
+# def solution(msg):
+#     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#     d = {k:v for (k,v) in zip(alphabet, list(range(1,27)))}
+#     answer = []
+
+#     while True:
+#         if msg in d:
+#             answer.append(d[msg])
+#             break
+#         for i in range(1, len(msg)+1):
+#             if msg[0:i] not in d:
+#                 answer.append(d[msg[0:i-1]])
+#                 d[msg[0:i]] = len(d)+1
+#                 msg = msg[i-1:]
+#                 break
+
+#     return answer
