98c71ea489eff6bd75e9118674ad4a6846c0b257
nicky <kwaknicky@naver.com>
2024-08-26 15:54:12 +0900

    [level 2] Title: 땅따먹기, Time: 204.60 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,12 +1,5 @@
 def solution(land):
-    answer = 0
-    dp = [[0 for _ in range(4)]for _ in range(len(land))]
-    
-    for i in range(len(land)):
+    for i in range(1, len(land)):
         for j in range(4):
-            if i == 0:
-                dp[i][j] = land[i][j]
-            else:
-                arr = dp[i-1][:j] + dp[i-1][j+1:]
-                dp[i][j] = land[i][j] + max(arr)
-    return max(dp[-1])
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+    return max(land[-1])
