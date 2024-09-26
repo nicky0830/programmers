5b5cb1508a7fe638b35c59d7877f83087e557648
nicky <kwaknicky@naver.com>
2024-09-26 15:41:40 +0900

    [level 2] Title: 타겟 넘버, Time: 496.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,11 +1,7 @@
-from itertools import combinations
 def solution(numbers, target):
-    answer = 0
-    minus = (sum(numbers) - target)/2
-    f = list(filter(lambda x: x<= minus, numbers))
-    for i in range(len(f)):
-        c = list(combinations(f, i+1))
-        for j in c:
-            if sum(j) == minus:
-                answer += 1
-    return answer
+    if not numbers and target == 0:
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
