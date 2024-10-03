2eae3d6a73a8acf16f850570b9615565dd484174
nicky <kwaknicky@naver.com>
2024-10-03 13:40:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,36 +1,24 @@
 from collections import deque
 
 def solution(maps):
-    dx = [-1,0,1,0]
-    dy = [0,1,0,-1]
+    x_move = [1, 0, -1, 0]
+    y_move = [0, 1, 0, -1]
 
-    result = 0
-
-    n = len(maps)
-    m = len(maps[0])
-
-    ck = [[0]*m for _ in range(n)]
-    dis = [[0]*m for _ in range(n)]
-    queue = deque()
-
-    ck[0][0] = 1
-    dis[0][0] = 1
-    queue.append((0,0))
+    x_h, y_h = (len(maps[0]), len(maps))
+    queue = deque([(0, 0, 1)])
 
     while queue:
-        now = queue.popleft()
+        x, y, d = queue.popleft()
 
         for i in range(4):
-            x = now[0]+dx[i]
-            y = now[1]+dy[i]
+            nx = x + x_move[i]
+            ny = y + y_move[i]
 
-            if 0<=x<=n-1 and 0<=y<=m-1:
-                if ck[x][y] == 0 and maps[x][y] == 1:
-                    ck[x][y] = 1
-                    dis[x][y] = dis[now[0]][now[1]] + 1
-                    queue.append((x,y))
+            if nx > -1 and ny > -1 and nx < x_h and ny < y_h:
+                if maps[ny][nx] == 1 or maps[ny][nx] > d + 1:
+                    maps[ny][nx] = d + 1
+                    if nx == x_h - 1 and ny == y_h - 1:
+                        return d + 1
+                    queue.append((nx, ny, d + 1))
 
-    result = -1 if dis[n-1][m-1] == 0 else dis[n-1][m-1]
-
-    return result
-
+    return -1
