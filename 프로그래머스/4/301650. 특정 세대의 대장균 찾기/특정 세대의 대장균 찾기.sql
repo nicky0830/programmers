26d5be5d844cb5faf940ffafb5496f90e7f71ba2
nicky <kwaknicky@naver.com>
2024-10-11 17:04:18 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -0,0 +1,7 @@
+SELECT e3.id
+FROM ecoli_data e1
+         JOIN ecoli_data e2
+              ON e1.id = e2.parent_id
+         JOIN ecoli_data e3
+              ON e2.id = e3.parent_id
+WHERE e1.parent_id IS NULL
