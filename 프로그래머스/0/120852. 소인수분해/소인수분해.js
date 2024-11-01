ea5b7779ed7a69ebc15e50cffd35dfaad7dec07b
nicky <kwaknicky@naver.com>
2024-11-01 14:08:27 +0900

    [level 0] Title: 소인수분해, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = [];
+    let i = 2
+    while(n > 1) {
+      if (n%i == 0) {
+        answer.push(i)
+        while(n%i==0) {
+            n = n/i
+        }
+      }
+      i++;
+    }
+    return answer;
+}
